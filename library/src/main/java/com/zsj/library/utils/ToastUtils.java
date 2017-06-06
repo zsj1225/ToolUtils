8b77ae621eab301825ab5bfa5e3c265c98af529c
zhushengjun <shengjun.zhu@itel-mobile.com>
2017-06-06 18:10:22 +0800

    Toast工具类

---
+++ library/src/main/java/com/zsj/library/utils/ToastUtils.java
@@ -0,0 +1,28 @@
+package com.zsj.library.utils;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+/**
+ * Toast工具
+ */
+public class ToastUtils {
+
+    /**
+     * @param context Context
+     * @param string  内容
+     */
+    public static void show(final Activity context, final String string) {
+        //判断是否为主线程
+        if ("main".equals(Thread.currentThread().getName())) {
+            Toast.makeText(context, string, Toast.LENGTH_SHORT).show();
+        } else {//如果不是，就用该方法使其在ui线程中运行
+            context.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(context, string, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+}
